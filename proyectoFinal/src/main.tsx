3b17113689a69335f1bcf9fbb73e53b5d270cbc6
santtiiojeda8 <ojedasi2004@gmail.com>
2024-10-22 10:16:02 -0300

    commit del seba

--- proyectoFinal/src/main.tsx
+++ proyectoFinal/src/main.tsx
@@ -10,10 +10,9 @@ import store from './redux/store/store';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      <App/>
-    </Provider>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </BrowserRouter>
-    
   </StrictMode>,
 )
