824f47bae9fa6c095e32c0fbedadaeae485842be
PauloGuidolin <paulog162003@gmail.com>
2024-10-22 09:11:22 -0300

    json de prueba

---
+++ proyectoFinal/persona.ts
@@ -0,0 +1,22 @@
+{
+  "personas": [
+    {
+      "id": 1,
+      "email": "persona1@example.com",
+      "firstName": "Juan",
+      "lastName": "Pérez",
+      "phoneNumber": "123456789",
+      "adress": "Calle 123",
+      "birthdate": "1995-01-01T00:00:00.000+00:00"
+    },
+    {
+      "id": 2,
+      "email": "persona2@example.com",
+      "firstName": "María",
+      "lastName": "González",
+      "phoneNumber": "987654321",
+      "adress": "Avenida 456",
+      "birthdate": "1990-05-15T00:00:00.000+00:00"
+    }
+  ]
+}
